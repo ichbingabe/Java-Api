c4b78c08e048f61edf27a2bcbad68a42c7b0a18b
Gabriel Esteves <38438278+ichbingabe@users.noreply.github.com>
2022-08-09 22:43:58 -0300

    Update ParkingSpotDto.java

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -2,7 +2,6 @@ package com.api.parkingcontrol.dtos;
 
 import com.api.parkingcontrol.models.CarModel;
 
-import javax.mail.Address;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
